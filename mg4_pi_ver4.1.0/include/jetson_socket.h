c920de04e39f8e33f2d7e53285e0dea08c37e878
Sh-kawai <kawaiprogram@gmail.com>
2023-12-15 23:39:04 +0900

    jetson_socket 文字列送信

--- mg4_pi_ver4.1.0/include/jetson_socket.h
+++ mg4_pi_ver4.1.0/include/jetson_socket.h
@@ -1,10 +1,41 @@
 #ifndef __JETSON_SOCKET__
 #define __JETSON_SOCKET__
 
-#define HOST ""
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define HOST "127.0.0.1"
 #define PORT 8080
 #define MAX_BUFFER_SIZE 1024
 
-class Server;
+class Server {
+public:
+  Server(const char* host, int port) : host_(host), port_(port) {}
+
+  // サーバーの接続
+  void s_open();
+  // メッセージ送受信
+  char* round_trip(const char s_msg[MAX_BUFFER_SIZE]);
+  // サーバーを閉じる
+  void s_close();
+
+private:
+  const char* host_;
+  int port_;
+  int serversock_;
+  int clientsock_;
+  char s_msg_[MAX_BUFFER_SIZE];
+  char rcvmsg_[MAX_BUFFER_SIZE];
+
+  // サーバーソケットの作成
+  void createServerSocket();
+  // サーバーソケットのバインドと接続待ちの設定
+  void bindAndListen();
+  // クライアントからの接続待ち
+  void acceptConnection();
+};
+
 
 #endif
