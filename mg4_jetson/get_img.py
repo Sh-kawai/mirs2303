9bb863c9d7e93efa01660c557a6c26d2fe450936
kawai <kawaiprogram@gmail.com>
2023-12-20 01:02:40 +0900

    動画&定期撮影の実装

--- mg4_jetson/get_img.py
+++ mg4_jetson/get_img.py
@@ -17,33 +17,41 @@ def save_img(capture, place=""):
     csv_handle.write(time=time_str, place=place)
     print(f"保存しました:{time_str}.png")
 
-def get_img(place="", loop=False, time_auto=False):
+def get_img(place="", time_auto=False):
     # カメラを起動
     capture = cv2.VideoCapture(0)
     if not capture.isOpened():
         print("エラー: カメラを開けませんでした。")
         return False
 
+    auto_time = 30
+    start_time = time.time()
     while(True):
         # 1フレームの画像を取得
         ret, frame = capture.read()
-        # 画像をウィンドウに表示
-        cv2.imshow("frame", frame)
-
+        
         keyInp = cv2.waitKey(1)
+        
+        # qを押されたら停止
+        if keyInp & 0xFF == ord('q'):
+            print("KeyInput q: finish get_img()")
+            break
 
         if time_auto:
-            save_img(capture=capture, place=place)
-            time.sleep(30)
+            elapsed_time = time.time() - start_time
+            remaining_time = int(auto_time - elapsed_time)
+            text = f"{remaining_time}"
+            cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            if remaining_time <= 0:
+                save_img(capture=capture, place=place)
+                start_time = time.time()
         #elif keyInp & 0xFF == 13:
         else:
             save_img(capture=capture, place=place)
-            if not loop:
-                break
-
-        # qを押されたら停止
-        if keyInp & 0xFF == ord('q'):
             break
+        
+        # 画像をウィンドウに表示
+        cv2.imshow("frame", frame)
 
     # 解放
     capture.release()
@@ -63,29 +71,31 @@ def save_movie():
     time_str = current_time.strftime('%Y-%m-%d_%H-%M-%S')
     output_file = os.path.join(JETSON_PATH, f"videos/{time_str}.mp4")
 
-    fource = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(output_file, fource, fps, (frame_width, frame_height))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_file, fourcc, fps, (frame_width, frame_height))
 
     record_duration = 30
     start_time = time.time()
 
     print("Recording for {} seconds...".format(record_duration))
 
-    while (time.time() - start_time) > record_duration:
+    while (time.time() - start_time) < record_duration:
         ret, frame = cap.read()
+        frame_cp = frame
 
         if not ret:
             print("Error: Failed to capture frame.")
             break
-
-        cv2.imshow("Frame", frame)
-
-        out.write(frame)
-
+        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        
+        text = f"{int(time.time() - start_time)}/{record_duration}"
+        cv2.putText(frame_cp, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+        cv2.imshow("Frame", frame_cp)
+        out.write(frame)
 
-        time.sleep(0.033)
+        #time.sleep(0.033)
 
     cap.release()
     out.release()
@@ -95,5 +105,5 @@ def save_movie():
     print("Recording completed. Video saved as", output_file)
 
 if __name__ == "__main__":
-    get_img(place="Dlab", loop=True, time_auto=True) 
-    #save_movie()
+    #get_img(place="Dlab", time_auto=True) 
+    save_movie()
