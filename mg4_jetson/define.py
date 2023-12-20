d0977221d76b54857967d9f618ab5bd9250fd877
kawai <kawaiprogram@gmail.com>
2023-12-20 18:31:27 +0900

    define

--- mg4_jetson/define.py
+++ mg4_jetson/define.py
@@ -1,4 +1,9 @@
+import os
+
 JETSON_PATH = "/home/mirs2303/mirs2303/mg4_jetson"
+PIC_CSV_PATH = os.path.join(JETSON_PATH, "pictures/picture_data.csv")
+VID_CSV_PATH = os.path.join(JETSON_PATH, "videos/video_data.csv")
+SCH_CSV_PATH = os.path.join(JETSON_PATH, "pschedule.csv")
 GDRIVE_FOLDER_ID = "1CONPdtwSL4rP9xj4kQDjPVK7Fi-I8GkE"
 SHEET_ID = "1e6MzBqjgl8S1QtGBeQOWqrg8rwIqeiarAl570BGflG0"
 SERVICE_JSON = "photo-test-393509-a09fbab1df81.json"
