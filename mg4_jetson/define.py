01daa19c5991bfd5107d2482d3a8d91f29b1c70b
kawai <kawaiprogram@gmail.com>
2023-12-24 18:08:41 +0900

    picture_dir

--- mg4_jetson/define.py
+++ mg4_jetson/define.py
@@ -2,7 +2,8 @@ import os
 
 #JETSON_PATH = "/home/mirs2303/mirs2303/mg4_jetson"
 JETSON_PATH = os.path.relpath(os.path.dirname(__file__))
-PIC_CSV_PATH = os.path.join(JETSON_PATH, "pictures/picture_data.csv")
+PICTURE_DIR = os.path.join(JETSON_PATH, "pictures")
+PIC_CSV_PATH = os.path.join(PICTURE_DIR, "picture_data.csv")
 VID_CSV_PATH = os.path.join(JETSON_PATH, "videos/video_data.csv")
 SCH_CSV_PATH = os.path.join(JETSON_PATH, "schedule_app/schedule.csv")
 G_FOLDER_MAIN_ID = "1CONPdtwSL4rP9xj4kQDjPVK7Fi-I8GkE"
